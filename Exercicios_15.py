260ce144655d8dd24b6d0a5928eb2fe418536494
Ian Victor <ianvictor.denv@gmail.com>
2022-12-07 09:02:46 -0300

    atualizado

--- Exercicios_15.py
+++ Exercicios_15.py
@@ -7,17 +7,17 @@
 # Triângulo Escaleno: três lados diferentes;
 
 
-lado1 = float(input("Lado 1: "))
-lado2 = float(input("Lado 2: "))
-lado3 = float(input("Lado 3: "))
+lado1 = float(input('Lado 1:---> '))
+lado2 = float(input('Lado 2:--> '))
+lado3 = float(input('Lado 3:--> '))
 
 if lado1 + lado2 > lado3 or lado1 + lado3 > lado2 or lado2 + lado3 > lado1:
-    print("É UM TRINGULO")
+    print('É UM TRINGULO')
     if lado1 == lado2 and lado1 == lado3:
-        print("Equilatero")
+        print('Equilatero')
     elif lado1 == lado2 or lado2 == lado3 or lado3 == lado1:
-        print("Isóceles")
+        print('Isóceles')
     else:
-        print("Escaleno")
+        print('Escaleno')
 else:
-    print("Não é um TRINGULO")
+    print("NÃO É UM TRINGULO")
